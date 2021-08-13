ae0af8911aad4d5e1fef29a4b5e418d23570e385
harmsm <harmsm@gmail.com>
2021-08-12 17:21:56 -0700

    prepping to make topiary a package

--- topiary/reconcile.py
+++ topiary/reconcile.py
@@ -1,57 +1,104 @@
 
-def create_generax_output(df,)
 
-fasta_out = []
+def annotate_tree_with_calls(df,tree,work_on_copy=True):
+    """
+    Annotate the leaves of an ete3 tree with information extracted from a
+    topiary dataframe.
+    """
 
-gene_tree_to_write = gene_tree.copy(method="deepcopy")
-gene_tree_to_write.resolve_polytomy()
-seen_in_gene_tree = []
+    # Copy tree -- do not operate on input tree directly
+    tree = util.load_tree(tree)
 
-link_dict = {}
-for l in gene_tree_to_write.get_leaves():
+    # Create dictionaries mapping uid to species, paralog, ott, and call.
+    out_dict = {}
+    for i in range(len(df)):
 
-    # For generating mapping file, record which uid are associated with what ott
-    try:
-        link_dict[l.ott].append(l.name)
-    except KeyError:
-        link_dict[l.ott] = [l.name]
+        uid = df.uid.iloc[i]
+        species = df.species.iloc[i]
+        ott = f"{df.ott.iloc[i]:d}"
+        paralog = df.paralog.iloc[i]
+        call = f"{ott}|{paralog}"
 
-    # Get sequence
-    aligned_seq = df.loc[df.uid == l.name,"alignment"].iloc[0]
+        out_dict[uid] = {"species":species,
+                         "paralog":paralog,
+                         "ott":ott,
+                         "call":call}
 
-    # Clean up alignment, removing non-aa characters
-    aligned_seq = re.sub("[^ACDEFGHIKLMNPQRSTVWYZ-]","-",aligned_seq)
-    fasta_out.append(f">{l.name}\n{aligned_seq}\n")
+    # Go through tree and assign leaves their calls etc.
+    for node in tree.get_leaves():
+        try:
+            for k in out_dict[node.name]:
+                node.add_feature(k,out_dict[node.name][k])
+        except KeyError:
+            err = f"uid {node.name} in tree but not data frame!\n"
+            raise ValueError(err)
 
-    # Record that we saw this ott
-    seen_in_gene_tree.append(l.ott)
+    return tree
 
+def create_generax(df,gene_tree):
+    """
+    Gene tree, expected to have uid taxon names.
+    """
 
-# Unique list of ott seen
-seen_in_gene_tree = list(set(seen_in_gene_tree))
+    # Only look at sequences flagged to keep
+    df = df.loc[df.keep].copy() # copy is to avoid assign-to-copy warning
 
-# Create fully resolved species tree
-species_tree_to_write = species_tree.copy(method="deepcopy")
-species_tree_to_write.prune(list(seen_in_gene_tree))
+    # Annotate gene tree with uid, ott, etc. and arbitrarily resolve any
+    # polytomies.
+    gene_tree = annotate_tree_with_calls(df,gene_tree)
+    gene_tree.resolve_polytomy()
 
-# Resolve polytomies and make sure all branch lenghts/supports have values
-species_tree.resolve_polytomy()
-for n in species_tree_to_write.traverse():
-    if n.dist != 1:
-        n.dist = 1
-    if n.support != 1:
-        n.support = 1
+    uid_in_gene_tree = []
+    link_dict = {}
+    for l in gene_tree.get_leaves():
 
-f = open("generax-test/alignment.fasta","w")
-f.write("".join(fasta_out))
-f.close()
+        # For generating mapping file, record which uid are associated with what ott
+        try:
+            link_dict[l.ott].append(l.name)
+        except KeyError:
+            link_dict[l.ott] = [l.name]
 
-gene_tree_to_write.write(outfile="generax-test/gene_tree.newick",format=5)
-species_tree_to_write.write(outfile="generax-test/species_tree.newick")
+        # Make sure this uid is actually in the df once and only once
+        if np.sum(df.uid == l.name) != 1:
+            err = f"tree taxon {l.name} either missing or duplicated in data frame\n"
+            raise ValueError(err)
 
-f = open("generax-test/mapping.link","w")
-for k in link_dict:
-    f.write(f"{k}:")
-    f.write(";".join(link_dict[k]))
-    f.write("\n")
-f.close()
+        # Record that we saw this uid
+        uid_in_gene_tree.append(l.name)
+
+    # Make df only have uid seen (will automatically trim down to only ott
+    # of interest)
+    mask = np.array([u in uid_in_gene_tree for u in df.uid],dtype=np.bool)
+    df = df.loc[mask]
+
+    # Get species tree.
+    dp_tree = topiary.get_species_tree(df)
+    dp_tree_str = dp_tree.as_string(schema="newick")
+    dp_tree_str = re.sub("\'","",dp_tree_str)
+    species_tree = ete3.Tree(dp_tree_str)
+
+    # Get species tree that has only the species from the gene tree
+    species_tree_to_write = species_tree.copy(method="deepcopy")
+    species_tree_to_write.prune(list(seen_in_gene_tree))
+
+    # Resolve polytomies and make sure all branch lenghts/supports have values
+    species_tree.resolve_polytomy()
+    for n in species_tree_to_write.traverse():
+        if n.dist != 1:
+            n.dist = 1
+        if n.support != 1:
+            n.support = 1
+
+    f = open("generax-test/alignment.fasta","w")
+    f.write("".join(fasta_out))
+    f.close()
+
+    gene_tree_to_write.write(outfile="generax-test/gene_tree.newick",format=5)
+    species_tree_to_write.write(outfile="generax-test/species_tree.newick")
+
+    f = open("generax-test/mapping.link","w")
+    for k in link_dict:
+        f.write(f"{k}:")
+        f.write(";".join(link_dict[k]))
+        f.write("\n")
+    f.close()
--- topiary/util.py
+++ topiary/util.py
@@ -8,10 +8,14 @@ utility functions for the topiary package
 
 from . import _private
 
+import ete3
+from ete3 import Tree
+
 import pandas as pd
 import numpy as np
 
 import re, sys, os, string, random, pickle, io, urllib, http
+import warnings
 
 def create_pipeline_dict():
     """
@@ -92,3 +96,86 @@ def uid_to_pretty(df,to_convert,out_file=None,overwrite=False):
     """
 
     return _private.convert(df,to_convert,out_file,overwrite,uid_to_pretty=True)
+
+
+def load_tree(tree,fmt=None):
+    """
+    Load a tree into an ete3 tree data structure.
+
+    tree: some sort of tree. can be an ete3.Tree (returns self), a dendropy
+          Tree (converts to newick and drops root), a newick file or a newick
+          string.
+    fmt: format for reading tree from newick.  0-9 or 100. See ete3 documentation
+         for how these are read (http://etetoolkit.org/docs/latest/tutorial/tutorial_trees.html#reading-and-writing-newick-trees).
+         As of ETE3.1.1, these numbers mean:
+
+
+         |        ======  ==============================================
+         |        FORMAT  DESCRIPTION
+         |        ======  ==============================================
+         |        0        flexible with support values
+         |        1        flexible with internal node names
+         |        2        all branches + leaf names + internal supports
+         |        3        all branches + all names
+         |        4        leaf branches + leaf names
+         |        5        internal and leaf branches + leaf names
+         |        6        internal branches + leaf names
+         |        7        leaf branches + all names
+         |        8        all names
+         |        9        leaf names
+         |        100      topology only
+         |        ======  ==============================================
+
+         if fmt is None, try to parse without a format descriptor, then these
+         formats in numerical order.
+
+    Returns an ete3 tree object.
+    """
+
+    # Already an ete3 tree.
+    if type(tree) is ete3.TreeNode:
+        return tree
+
+    # Convert dendropy tree into newick (drop root)
+    if type(tree) is dp.Tree:
+        tree = tree.as_string(schema="newick",suppress_rooting=True)
+
+    # If we get here, we need to convert. If fmt is not specified, try to parse
+    # without a format string.
+    if fmt is None:
+
+
+        try:
+            t = Tree(tree)
+        except ete3.parser.newick.NewickError:
+
+            # Try all possible formats now, in succession
+            w = "\n\nCould not parse tree without format string. Going to try different\n"
+            w += "formats. Please check output carefully.\n\n"
+            warnings.warn(w)
+
+            formats = list(range(10))
+            formats.append(100)
+
+            t = None
+            for f in formats:
+                try:
+                    t = Tree(tree,format=f)
+                    w = f"\n\nSuccessfully parsed tree with format style {f}.\n"
+                    w += "Please see ete3 documentation for details:\n\n"
+                    w += "http://etetoolkit.org/docs/latest/tutorial/tutorial_trees.html#reading-and-writing-newick-trees\n\n"
+                    warnings.warn(w)
+                    break
+
+                except ete3.parser.newick.NewickError:
+                    continue
+
+            if t is None:
+                err = "\n\nCould not parse tree!\n\n"
+                raise ValueError(err)
+
+    else:
+        # Try a conversion with the specified format
+        t = Tree(tree,format=fmt)
+
+    return t
