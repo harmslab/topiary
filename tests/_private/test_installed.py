f54c406579f930dffe836b85a1998d699183c985
Michael Harms <harmsm@gmail.com>
2022-06-29 09:07:45 -0700

    add module checking the versions of external binaries

---
+++ tests/_private/test_installed.py
@@ -0,0 +1,112 @@
+
+import pytest
+
+from topiary._private import installed
+
+import warnings
+
+def test__version_checker():
+
+    # Good check -- should work
+    cmd = ["git","--version"]
+    def _version_slicer(ret):
+        return ret.stdout.decode().split()[2].strip()
+
+    v = installed._version_checker(cmd,_version_slicer)
+    assert type(v) is tuple
+    assert len(v) > 1
+    assert int(v[0]) > 0
+
+    # Should fail with binary not found (-2,-2,-2)
+    cmd = ["not_really_a_binary"]
+    def _version_slicer(ret):
+        return ret.stdout.decode().split()[2].strip()
+
+    v = installed._version_checker(cmd,_version_slicer)
+    assert type(v) is tuple
+    assert v == (-2,-2,-2)
+
+    # Should fail with could not run (-1,-1,-1)
+    cmd = ["git","--bad_argument_not_recognized"]
+    def _version_slicer(ret):
+        return ret.stdout.decode().split()[2].strip()
+
+    v = installed._version_checker(cmd,_version_slicer)
+    assert type(v) is tuple
+    assert v == (-1,-1,-1)
+
+    # Should fail with ran but could not get version (0,0,0)
+    cmd = ["git","--version"]
+    def _version_slicer(ret):
+        # bad parsing call -- last split()[1] will throw an IndexError
+        return ret.stdout.decode().split()[2].strip().split()[1]
+
+    v = installed._version_checker(cmd,_version_slicer)
+    assert type(v) is tuple
+    assert v == (0,0,0)
+
+def test_check_muscle():
+
+    version = installed.check_muscle()
+
+    if version == (-2,-2,-2):
+        warings.warn("muscle not installed -- skipping test")
+
+    if version == (-1,-1,-1):
+        raise RuntimeError("muscle is installed but not working!")
+
+    if version == (0,0,0):
+        raise RuntimeError("muscle is installed but we cannot parse its version string!")
+
+
+def test_check_generax():
+
+    version = installed.check_generax()
+
+    if version == (-2,-2,-2):
+        warings.warn("generax not installed -- skipping test")
+
+    if version == (-1,-1,-1):
+        raise RuntimeError("generax is installed but not working!")
+
+    if version == (0,0,0):
+        raise RuntimeError("generax is installed but we cannot parse its version string!")
+
+def test_check_raxml():
+
+    version = installed.check_raxml()
+
+    if version == (-2,-2,-2):
+        warings.warn("raxml-ng not installed -- skipping test")
+
+    if version == (-1,-1,-1):
+        raise RuntimeError("raxml-ng is installed but not working!")
+
+    if version == (0,0,0):
+        raise RuntimeError("raxml-ng is installed but we cannot parse its version string!")
+
+def test_check_blastp():
+
+    version = installed.check_blastp()
+
+    if version == (-2,-2,-2):
+        warings.warn("blastp not installed -- skipping test")
+
+    if version == (-1,-1,-1):
+        raise RuntimeError("blastp is installed but not working!")
+
+    if version == (0,0,0):
+        raise RuntimeError("blastp is installed but we cannot parse its version string!")
+
+def test_check_makeblastdb():
+
+    version = installed.check_makeblastdb()
+
+    if version == (-2,-2,-2):
+        warings.warn("makeblastdb not installed -- skipping test")
+
+    if version == (-1,-1,-1):
+        raise RuntimeError("makeblastdb is installed but not working!")
+
+    if version == (0,0,0):
+        raise RuntimeError("makeblastdb is installed but we cannot parse its version string!")
---
+++ topiary/_private/installed.py
@@ -0,0 +1,156 @@
+"""
+Check for installed external software in the path.
+"""
+
+import subprocess
+
+def _version_checker(cmd,version_slicer):
+    """
+    Generic version checking function.
+
+    Parameters
+    ----------
+    cmd : list
+        command to pass via subprocess.run
+    version_slicer : function
+        function that takes the subprocess return value and pulls a version
+        string out of the captured stdout/stderr.
+
+    Returns
+    -------
+    version : tuple
+        output meanings:
+        + :code:`(-2,-2,-2)`, not found
+        + :code:`(-1,-1,-1)`, found but does not run
+        + :code:`(0,0,0)` found but could not figure out version
+        + :code:`(major,minor,patch)` i.e. (3.8.1). This is done by splitting on
+          the :code:`.` character, so this will always be a tuple but may have
+          any length > 1. Also, the elements will be :code:`str` not :code:`int`.
+    """
+
+    try:
+        ret = subprocess.run(cmd,capture_output=True)
+    except FileNotFoundError:
+        return (-2,-2,-2)
+
+    if ret.returncode != 0:
+        return (-1,-1,-1)
+
+    try:
+        version = version_slicer(ret)
+        if version[0] in ["v","V"]:
+            version = version[1:]
+        version = tuple(version.split("."))
+    except:
+        return (0,0,0)
+
+    return version
+
+
+def check_muscle():
+    """
+    Check for muscle in the PATH and get its version.
+
+    Returns
+    -------
+    version : tuple
+        output meanings:
+        + :code:`(-2,-2,-2)`, not found
+        + :code:`(-1,-1,-1)`, found but does not run
+        + :code:`(0,0,0)` found but could not figure out version
+        + :code:`(major,minor,patch)` i.e. (3.8.1). This is done by splitting on
+          the :code:`.` character, so this will always be a tuple but may have
+          any length > 1. Also, the elements will be :code:`str` not :code:`int`.
+    """
+
+    def _version_slicer(ret):
+        return ret.stdout.split()[1].decode()
+
+    return _version_checker(["muscle","-version"],_version_slicer)
+
+
+def check_generax():
+    """
+    Check for generax in the PATH and get its version.
+
+    Returns
+    -------
+    version : tuple
+        output meanings:
+        + :code:`(-2,-2,-2)`, not found
+        + :code:`(-1,-1,-1)`, found but does not run
+        + :code:`(0,0,0)` found but could not figure out version
+        + :code:`(major,minor,patch)` i.e. (3.8.1). This is done by splitting on
+          the :code:`.` character, so this will always be a tuple but may have
+          any length > 1. Also, the elements will be :code:`str` not :code:`int`.
+    """
+
+    def _version_slicer(ret):
+        return ret.stdout.decode().split("\n")[0].split()[2:][0]
+
+    return _version_checker(["generax"],_version_slicer)
+
+
+def check_raxml():
+    """
+    Check for raxml-ng in the PATH and get its version.
+
+    Returns
+    -------
+    version : tuple
+        output meanings:
+        + :code:`(-2,-2,-2)`, not found
+        + :code:`(-1,-1,-1)`, found but does not run
+        + :code:`(0,0,0)` found but could not figure out version
+        + :code:`(major,minor,patch)` i.e. (3.8.1). This is done by splitting on
+          the :code:`.` character, so this will always be a tuple but may have
+          any length > 1. Also, the elements will be :code:`str` not :code:`int`.
+    """
+
+    def _version_slicer(ret):
+        return ret.stdout.decode().strip().split("\n")[0].split()[2]
+
+    return _version_checker(["raxml-ng"],_version_slicer)
+
+
+def check_blastp():
+    """
+    Check for blastp in the PATH and get its version.
+
+    Returns
+    -------
+    version : tuple
+        output meanings:
+        + :code:`(-2,-2,-2)`, not found
+        + :code:`(-1,-1,-1)`, found but does not run
+        + :code:`(0,0,0)` found but could not figure out version
+        + :code:`(major,minor,patch)` i.e. (3.8.1). This is done by splitting on
+          the :code:`.` character, so this will always be a tuple but may have
+          any length > 1. Also, the elements will be :code:`str` not :code:`int`.
+    """
+
+    def _version_slicer(ret):
+        return ret.stdout.decode().split()[1].strip()
+
+    return _version_checker(["blastp","-version"],_version_slicer)
+
+def check_makeblastdb():
+    """
+    Check for makeblastdb in the PATH and get its version.
+
+    Returns
+    -------
+    version : tuple
+        output meanings:
+        + :code:`(-2,-2,-2)`, not found
+        + :code:`(-1,-1,-1)`, found but does not run
+        + :code:`(0,0,0)` found but could not figure out version
+        + :code:`(major,minor,patch)` i.e. (3.8.1). This is done by splitting on
+          the :code:`.` character, so this will always be a tuple but may have
+          any length > 1. Also, the elements will be :code:`str` not :code:`int`.
+    """
+
+    def _version_slicer(ret):
+        return ret.stdout.decode().split()[1].strip()
+
+    return _version_checker(["makeblastdb","-version"],_version_slicer)
