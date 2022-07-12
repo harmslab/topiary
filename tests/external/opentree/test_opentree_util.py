423b8d6fc1d40748838a28f7bd9e5825c8cb5867
Michael Harms <harmsm@gmail.com>
2022-07-12 09:56:15 -0700

    added opentre unit testing

--- tests/external/opentree/test_opentree_util.py
+++ tests/external/opentree/test_opentree_util.py
@@ -54,3 +54,43 @@ def test_get_phylo_context():
         print(f"Trying bad species list {b}")
         with pytest.raises(ValueError):
             opentree.util.get_phylo_context(b)
+
+def test_is_allowed_phylo_context():
+
+    assert "Animals" == opentree.util.is_allowed_phylo_context("Animals")
+
+    with pytest.raises(ValueError):
+        opentree.util.is_allowed_phylo_context("not_real_context")
+
+
+def test_species_to_ott():
+
+    species_list = ["Homo sapiens",
+                    "Thermus thermophilus",
+                    "Methanococcus voltae",
+                    "Saccharomyces cerevisiae"]
+
+    expected_ott = [770315,276534,565131,356221]
+
+    out = opentree.util.species_to_ott(species_list,phylo_context="All life")
+    assert len(out[1]) == 0
+    for i, s in enumerate(species_list):
+        assert out[0][s][0] == expected_ott[i]
+
+    bad_species_list = [None,[1.5,2.5],1,{1:"test"},list,int,str,"SPECIES"]
+    for b in bad_species_list:
+        print(f"Trying bad species list {b}")
+        with pytest.raises(ValueError):
+            opentree.util.species_to_ott(b)
+
+    out = opentree.util.species_to_ott(["Nannospalax galili"],
+                                        phylo_context="All life")
+    # will throw a key error if regex not properly stripping "(in domain Bacteria)"
+    out[0]["Nannospalax galili"]
+    assert out[0]["Nannospalax galili"][1] == "Nannospalax galili"
+
+    # Make sure phylo_context is really working. This species name is ambiguous;
+    # this checks to see if got correct one
+    out = opentree.util.species_to_ott(["Nannospalax galili"],
+                                        phylo_context="Bacteria")
+    assert out[0]["Nannospalax galili"][0] == 5909124
