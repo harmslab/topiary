891e9db046e36b8669f5f579d36e8fe81d8120ca
Michael Harms <harmsm@gmail.com>
2022-08-05 12:33:27 -0700

    cleaning up gh workflow and environment setup

--- setup.py
+++ setup.py
@@ -9,33 +9,32 @@ from setuptools import find_packages, setup
 
 # Package meta-data.
 DESCRIPTION = \
-"""Python framework for doing ancestral sequence reconstruction using pandas
-dataframes and ete3 trees as the primary data structures."""
+"""Python framework for doing ancestral sequence reconstruction."""
 URL = "https://github.com/harmslab/topiary"
 EMAIL = "harmsm@gmail.com"
 AUTHOR = "Michael J. Harms"
 REQUIRES_PYTHON = ">=3.8.0"
 VERSION = None
 
-# What packages are required for this module to be executed?
-REQUIRED = [
-
-    # General scientific computing and display
-    "numpy",
-    "pandas>=1.3.1",
-    "xlrd",
-    "openpyxl",
-    "matplotlib>=3.4.2",
-    "tqdm",
-
-    # bioinformatics
-    "biopython>=1.78",
-    "ete3>=3.1.2",
-    "opentree>=1.0.1",
-    "dendropy>=4.5.2",
-    "pastml>=1.9.34",
-    "toytree"
-]
+# # What packages are required for this module to be executed?
+# REQUIRED = [
+#
+#     # General scientific computing and display
+#     "numpy",
+#     "pandas>=1.3.1",
+#     "xlrd",
+#     "openpyxl",
+#     "matplotlib>=3.4.2",
+#     "tqdm",
+#
+#     # bioinformatics
+#     "biopython>=1.78",
+#     "ete3>=3.1.2",
+#     "opentree>=1.0.1",
+#     "dendropy>=4.5.2",
+#     "pastml>=1.9.34",
+#     "toytree"
+# ]
 
 
 here = os.path.abspath(os.path.dirname(__file__))
@@ -65,10 +64,10 @@ setup(
     python_requires=REQUIRES_PYTHON,
     url=URL,
     packages=find_packages(exclude=('tests',)),
-    install_requires=REQUIRED,
-    extras_require = {
-        'test': ['pytest'],
-    },
+    #install_requires=REQUIRED,
+    #extras_require = {
+    #    'test': ['pytest'],
+    #},
     scripts=glob.glob("bin/topiary-*"),
     package_data={'': ['*.csv']},
     include_package_data=True,
@@ -78,7 +77,7 @@ setup(
       'Intended Audience :: Science/Research',
       'Programming Language :: Python :: 3.8',
       'Programming Language :: Python :: 3.9',
+      'Programming Language :: Python :: 3.10',
     ],
     keywords="phylogenetics; ancestral sequence reconstruction; ASR; bioinformatics; protein; evolutionary biochemistry"
 )
-
