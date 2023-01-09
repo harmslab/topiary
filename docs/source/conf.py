b7f0f97cc1f843cea0f372d608d68f359001a35b
Michael Harms <harmsm@gmail.com>
2023-01-09 05:34:02 -0800

    updating docs so icon appears on readthedocs (sphinx bug workaround)

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -61,9 +61,9 @@ html_theme = 'sphinx_rtd_theme'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
-html_logo = "_static/img/logo_150x150.png"
+html_logo = "logo_150x150.png"
 
-html_favicon = "_static/img/favicon.ico"
+html_favicon = "favicon.ico"
 
 html_css_files = [
     'css/stylesheet.css',
