a135fcf6c612b254b8cb8b2425f8291118b9d486
Chris <17653365+odcambc@users.noreply.github.com>
2023-01-25 12:31:01 -0800

    Sets entrez API key from environment

--- topiary/ncbi/__init__.py
+++ topiary/ncbi/__init__.py
@@ -5,6 +5,12 @@ Interface to NCBI blast and entrez.
 from Bio import Entrez
 Entrez.email = "topiary.phylogenetics@gmail.com"
 
+import os
+try:
+    Entrez.api_key = os.environ['NCBI_API_KEY']
+except KeyError:
+    pass
+
 from ._parse_ncbi_line import parse_ncbi_line
 from .blast import local_blast, ncbi_blast, recip_blast, make_blast_db
 from .blast import records_to_df, read_blast_xml
